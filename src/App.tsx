55b5582629d289106bd6598d4c6c7eab1fac43f1
Stanley-Felix-Bergamo <stanley.felix.bergamo@outlook.com>
2023-05-30 16:53:26 -0300

    feat: criando estilo global em styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+import EstiloGlobal from './styles'
 function App() {
   return (
     <div className="App">
+      <EstiloGlobal />
       <h1>Olá, Mundo !</h1>
     </div>
   )
---
+++ src/styles/index.ts
@@ -0,0 +1,15 @@
+import styled, { createGlobalStyle } from 'styled-components'
+
+const EstiloGlobal = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    list-style: none;
+    text-decoration: none;
+    font-family: 'Roboto', sans-serif
+
+  }
+`
+
+export default EstiloGlobal
